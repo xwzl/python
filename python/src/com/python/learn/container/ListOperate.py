40177ee5021e691a3497e3a5d2794fc157c88f89
徐伟智 <l>
2019-10-09 09:48:00 +0800

    list 列表注意事项

--- python/src/com/python/learn/container/ListOperate.py
+++ python/src/com/python/learn/container/ListOperate.py
@@ -1,3 +1,5 @@
+import gc
+
 # 在实际开发中，经常需要将一些（不只一个）数据暂储起来，以便将来使用。说到这里，一些读者可能知道或听说过数组，它就可以把多个数据挨个存储到一起，通过数组下标可以访问数组中的
 # 各个元素。但使用数组存储数据有一个弊端，就是所存储的数据必须类型一致。
 #
@@ -21,7 +23,7 @@
 #       type(["c.biancheng.net" , 1 , [2,3,4] , 3.0])
 #
 # 可以看到，它的数据类型为 list，就表示它是一个列表。
-print("---------------------创建列表的两种形式---------------------")
+print("\n---------------------创建列表的两种形式---------------------")
 
 # 使用 = 运算符直接创建列表
 #
@@ -65,7 +67,7 @@ print(names)
 del names  # 删除后的列表无法继续使用
 # print(names)
 
-print("---------------------列表添加元素---------------------")
+print("\n---------------------列表添加元素---------------------")
 
 # 列表添加元素
 #
@@ -121,7 +123,7 @@ inserts.insert(1, ["CG", "IG"])
 print(inserts)
 print("listName.insert(index,element) 向指定位置插入元素，目测性能不好")
 
-print("---------------------列表添加元素---------------------\n")
+print("\n---------------------列表添加元素---------------------\n")
 
 # Python list列表删除元素（3种方法）
 #
@@ -166,7 +168,7 @@ print(c_list.count(0))  # 获取列表中元素的个数
 c_list.clear()
 print(c_list)
 
-print("---------------------修改元素---------------------\n")
+print("\n---------------------修改元素---------------------\n")
 
 # 列表的元素相当于变量，因此程序可以对列表的元素赋值，这样即可修改列表的元素
 a_list = [2, 4, -3.4, 'crazyIt', 23]
@@ -207,3 +209,144 @@ c_list = list(range(1, 10))
 # 指定step为2，被赋值的元素有4个，因此用于赋值的列表也必须有4个元素
 c_list[2: 9: 2] = ['a', 'b', 'c', 'd']
 print(c_list)  # [1, 2, 'a', 4, 'b', 6, 'c', 8, 'd']
+
+print("\n---------------------count()方法---------------------\n")
+a_list = [2, 30, 'a', [5, 30], 30]
+# 计算列表中30的出现次数
+print(a_list.count(30))
+# 计算列表中[5, 30]的出现次数
+print(a_list.count([5, 30]))
+
+print("\n---------------------index()用法---------------------\n")
+# index() 方法用于定位某个元素在列表中出现的位置（也就是索引），如果该元素没有出现，则会引发 ValueError 错误。
+a_list = [2, 30, 'a', 'b', 'crazyIt', 30]
+# 定位元素30的出现位置
+print(a_list.index(30))
+# 从索引2处开始、定位元素30的出现位置
+print(a_list.index(30, 1))
+# 从索引2处到索引4处之间定位元素30的出现位置，因为找不到该元素，会引发 ValueError 错误
+print(a_list.index(30, 0, 4))
+
+print("\n---------------------pop()用法---------------------\n")
+# pop() 方法会移除列表中指定索引处的元素，如果不指定，默认会移除列表中最后一个元素。
+p_list = [1, 2, 3, 4, 5, 6]
+print(p_list.pop())
+print(p_list)
+print(p_list.pop(0))
+
+# 在其他编程语言所实现的“栈”中，往往会提供一个 push() 方法，用于实现入栈操作，但 Python 的列表并没有提供 push() 方法，我们可以使用 append() 方法来代替 push() 方法实现入栈操作。
+stack = []
+# 向栈中“入栈”3个元素
+stack.append("fkit")
+stack.append("crazyit")
+stack.append("Charlie")
+print(stack)  # ['fkit', 'crazyit', 'Charlie']
+# 第一次出栈：最后入栈的元素被移出栈
+print(stack.pop())
+print(stack)  # ['fkit', 'crazyit']
+# 再次出栈
+print(stack.pop())
+print(stack)  # ['fkit']
+
+a_list = list(range(1, 8))
+# 将a_list列表元素反转
+a_list.reverse()
+print(a_list)
+
+# sort() 方法用于对列表元素进行排序，排序后原列表中的元素顺序会方发生改变。sort() 方法的语法格式如下：
+# listname.sort(key=None, reserse=False)
+#
+# 可以看到，和其他方法不同，此方法中多了 2 个参数，它们的作用分别是：
+# key 参数用于指定从每个元素中提取一个用于比较的键。例如，使用此方法时设置 key=str.lower 表示在排序时不区分字母大小写。
+# reverse 参数用于设置是否需要反转排序，默认 False 表示从小到大排序；如果将该参数设为 True，将会改为从大到小排序。
+print("\n---------------------sort()用法---------------------\n")
+a_list = [3, 4, -2, -30, 14, 9.3, 3.4]
+# 对列表元素排序
+a_list.sort()
+print(a_list)
+b_list = ['Python', 'Swift', 'Ruby', 'Go', 'Kotlin', 'Erlang']
+# 对列表元素排序：默认按字符串包含的字符的编码大小比较
+b_list.sort()
+print(b_list)  # ['Erlang', 'Go', 'Kotlin', 'Python', 'Ruby', 'Swift']
+
+# 注意，采用 sort() 方法对列表进行排序时，对中文支持不好，其排序结果与常用的音序排序法或者笔画排序法都不一致，因此，如果需要实现对中文内容的列表排序，
+# 还需要重新编写相应的方法进行处理，而不能直接使用 sort() 方法。
+b_list = ['Python', 'Swift', 'Ruby', 'Go', 'Kotlin', 'Erlang']
+# 指定key为len，指定使用len函数对集合元素生成比较的键，也就是按字符串的长度比较大小
+b_list.sort(key=len)
+print(b_list)
+# 指定反向排序
+b_list.sort(key=len, reverse=True)
+print(b_list)
+
+# Python list添加元素的方法及区别
+# 定义两个列表（分别是 list1 和 list3），并分别使用 +、extend()、append() 对这两个 list 进行操作，其操作的结果赋值给 l2。
+#
+# 根据输出结果，可以分析出以下几个结论：
+#
+#   使用“+”号连接的列表，是将 list3 中的元素放在 list 的后面得到的 l2。并且 l2 的内存地址值与 list1 并不一样，这表明 l2 是一个重新生成的列表。
+#   使用 extend 处理后得到的 l2 是 none。表明 extend 没有返回值，并不能使用链式表达式。即 extend 千万不能放在等式的右侧，这是编程时常犯的错误，一定要引起注意。
+#   extend 处理之后， list1 的内容与使用“+”号生成的 l2 是一样的。但 list1 的地址在操作前后并没有变化，这表明 extend 的处理仅仅是改变了 list1，而没有重新创建一个 list。
+#   从这个角度来看，extend 的效率要高于“+”号。
+#   从 append 的结果可以看出，append 的作用是将 list3 整体当成一个元素追加到 list1 后面，这与 extend 和“+”号的功能完全不同，这一点也需要注意。
+#
+# extend 和 + 效果一样，但是 + 号会产生新的 list 对象效率较低，extend 方法并不会产生新的对象。append 的缺点就是把对象当成一个元素处理
+tt = 'hello'
+# 定义一个包含多个类型的 list
+list1 = [1, 4, tt, 3.4, "yes", [1, 2]]
+print(list1, id(list1))
+print("1.----------------")
+# 比较 list 中添加元素的几种方法的用法和区别
+list3 = [6, 7]
+l2 = list1 + list3
+print(l2, id(l2))
+print("2.----------------")
+l2 = list1.extend(list3)
+print(l2, id(l2))
+print(list1, id(list1))
+print("3.----------------")
+l2 = list1.append(list3)
+print(l2, id(l2))
+print(list1, id(list1))
+
+# Python list删除操作
+# 这 3 行输出分别是 list1 的原始内容、删除一部分切片内容、删除指定索引内容。可以看到，del 关键字按照指定的位置删掉了指定的内容。
+tt = 'hello'
+# 定义一个包含多个类型的 list
+list1 = [1, 4, tt, 3.4, "yes", [1, 2]]
+print(list1)
+del list1[2:5]
+print(list1)
+del list1[2]
+print(list1)
+
+# 需要注意的是，在使用 del 关键字时，一定要搞清楚，删除的到底是变量还是数据。例如，下面代码演示和删除变量的方法：
+tt = 'hello'
+# 定义一个包含多个类型的 list
+list1 = [1, 4, tt, 3.4, "yes", [1, 2]]
+l2 = list1
+print(id(l2), id(list1))
+# 第一行输出的内容是 l2 和 list1 的地址，可以看到它们是相同的，说明 l2 和 list1 之间的赋值仅仅是传递内存地址。接下来将 list1 删掉，并打印 l2，可以看到，l2 所指向的
+# 内存数据还是存在的，这表明 del 删除 list1 时仅仅是销毁了变量 list1，并没有删除指定的数据。
+del list1
+print(l2)
+print(list1)
+
+# 除了删除变量，其他的删除都是删除数据，比如将列表中数据全部清空
+tt = 'hello'
+# 定义一个包含多个类型的 list
+list1 = [1, 4, tt, 3.4, "yes", [1, 2]]
+l2 = list1
+l3 = l2
+del l2[:]
+print(l2)
+print(l3)
+
+# 另外，在实际过程中，即便使用 del 关键字删除了指定变量，且该变量所占用的内存再没有其他变量使用，此内存空间也不会真正地被系统回收并进行二次使用，它只是会被标记为无效内存。
+# 如果想让系统回收这些可用的内存，需要借助 gc 库中的 collect() 函数。例如：
+tt = 'hello'
+# 定义一个包含多个类型的 list
+list1 = [1, 4, tt, 3.4, "yes", [1, 2]]
+del list1
+# 回收内存地址
+gc.collect()
