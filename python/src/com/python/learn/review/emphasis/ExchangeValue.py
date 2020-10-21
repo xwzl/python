54dde6a8a1d5fc402436bbdd86443c906238ea9b
徐伟智 <624244232@qq.com>
2020-10-21 23:22:38 +0800

    day fou last

--- python/src/com/python/learn/review/emphasis/ExchangeValue.py
+++ python/src/com/python/learn/review/emphasis/ExchangeValue.py
@@ -1,4 +1,5 @@
 # python 中特有写法，交互数据
+import json
 
 x, y = 100, 200
 
@@ -8,3 +9,14 @@ y = t
 
 x, y = y, x
 print("x value {} , y value {}".format(x, y))
+
+students = ["张三", "李四", "王五"]
+dumps = json.dumps(students)  # list 转 json 字符串
+print(dumps)
+
+loads = json.loads(dumps)  # json 字符串转 list
+print(type(loads))
+
+# python 中执行代码
+eval("print(\"hello python\")")
+
